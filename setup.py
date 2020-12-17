cf158b894ba5b02dcf0bbc8aec2ca679545c651a
劉定威 <tingwei.liu.19930301@gmail.com>
2020-12-17 12:25:37 +0800

    第一版 含 1. js like dict 2. function logger

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    url='',
+    name='werdsazxc',
+    version='1.0.0',
+    description='Tools For Improve Effeciveness Of Project Develop',
+    author='Werdsazxc',
+    author_email='willy11342002@gmail.com',
+    py_modules=['werdsazxc'],
+)
---
+++ werdsazxc.py
@@ -0,0 +1,60 @@
+from inspect import signature
+from functools import wraps
+import traceback
+import requests
+import logging
+import inspect
+import json
+import sys
+import re
+logger = logging.getLogger('Tools')
+
+
+class Dict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, value in self.items():
+            if isinstance(value, dict):
+                self[key] = Dict(value)
+            if type(value) == list:
+                self[key] = [
+                    Dict(v) if isinstance(value, dict) else v
+                    for v in value
+                ]
+
+    def __getattr__(self, key):
+        if key.startswith('_'):
+            return dict.__getattr__(self, key)
+        return self.get(key)
+
+    def __setattr__(self, key, value):
+        if key.startswith('_'):
+            return dict.__setattr__(self, key, value)
+        self[key] = value
+
+    def __delattr__(self, key):
+        del self[key]
+
+
+def log(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        # 紀錄傳入參數
+        sig = signature(func)
+        bound = sig.bind_partial(*args, **kw)
+        bound.apply_defaults()
+        logger.debug(f'{func.__name__} 傳入: {dict(bound.arguments)}')
+
+        # 執行函數
+        try:
+            result = func(*args, **kw)
+        except Exception as e:
+            logger.error(f'{func.__name__} 錯誤: {e.__class__.__name__} {e}')
+            logger.error('\n' + traceback.format_exc())
+            raise e
+
+        # 紀錄結果
+        logger.debug(f'{func.__name__} 返回: {result}')
+        return result
+    return wrapper
+
