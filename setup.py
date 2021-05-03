41ef13e38e80575cfc69e7d8308aa65e52f09019
劉定威 <tingwei.liu.19930301@gmail.com>
2021-05-03 09:37:53 +0800

    依賴調整

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 setup(
     url='https://github.com/willy11342002/werdsazxc',
     name='werdsazxc',
-    version='1.7.2',
+    version='1.7.3',
     description='Tools For Improve Effeciveness Of Project Develop',
     author='Werdsazxc',
     author_email='willy11342002@gmail.com',
     py_modules=['werdsazxc'],
     install_requires=[
         'pycryptodome==3.9.8',
-        'PyYAML==5.1.2'
+        'PyYAML>=5.1.2'
     ]
 )
