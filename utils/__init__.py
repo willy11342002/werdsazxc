a906eb470db3ba088a3c086a8e39603bff1d9733
METAEDGE-TW\William.Liu <William.Liu@MetaEdge.com.tw>
2022-06-28 16:30:38 +0800

    fixed unnecessery duty bug error.

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import pycommon as common
-from . import pycryptor as cryptor
-from . import pydict as dict
-from . import pykeyboard as keyboard
-from . import pylogging as logging
-from . import pysystray as systray
